5a77cd4567ceb5069699abff2f36aa76b7049d62
eiliya keshtkar <e1l1yasec@gmail.com>
2023-03-21 18:08:52 +0330

    function find_in_sql_login improved

--- model/sql_helpers.php
+++ model/sql_helpers.php
@@ -1,32 +1,40 @@
 <?php
 
-function find_in_sql_login($user_,$pass_):bool{
-    $servername = "localhost";
-    $username = "am1";
-    $password = "A@mir881401";
-    $db_name = "vpn_am1ral1";
-//        $us=;
-    $conn = new mysqli($servername, $username, $password,$db_name);
+function find_in_sql_login($user_, $pass_) {
+  // Create connection
+  $conn = mysqli_connect("localhost", "am1", "A@mir881401", "vpn_am1ral1");
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+  // Check connection
+  if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
 
-    $stmt = $conn->prepare("SELECT * FROM users WHERE username=?&pass=?");
-    $stmt->bind_param("s", $user_,$pass_);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    if ($result->num_rows>0){
-        return true;
-        mysqli_close($conn);
+  // Prepare a statement
+  $stmt = mysqli_prepare($conn, "SELECT * FROM users WHERE username = ? AND password = ?");
 
-    }else{
-        return false;
-        mysqli_close($conn);
+  // Bind parameters
+  mysqli_stmt_bind_param($stmt, "ss", $user_, $pass_);
 
-    }
+  // Execute statement
+  mysqli_stmt_execute($stmt);
 
+  // Get result
+  $result = mysqli_stmt_get_result($stmt);
+
+  // Check if user exists
+  if ($result->num_rows > 0) {
+    // User exists
+    mysqli_stmt_close($stmt);
+    mysqli_close($conn);
+    return true;
+  } else {
+    // User does not exist
+    mysqli_stmt_close($stmt);
+    mysqli_close($conn);
+    return false;
+  }
 }
+
 function get_user_ac($usr_name):string{
     $servername = "localhost";
     $username = "am1";

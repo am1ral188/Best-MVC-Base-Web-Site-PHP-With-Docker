94be34d9e618df4f32adbc2586e0ba6994db7f51
am1ral188 <amiralizeinalzadeh0@gmail.com>
2023-04-13 01:26:12 +0330

    Initil commit

---
+++ docker/html/config.php
@@ -0,0 +1,12 @@
+<?php
+const site_root="http://localhost:1080/";
+const DB_HOST="localhost";
+const DB_USER="am1";
+const DB_PASS="A@mir881401";
+const DB_NAME="vpn_am1ral1";
+const SAVE_IMAGE_PATH="/tmp/";
+
+
+const APP_DEBUG = false;
+
+date_default_timezone_set("Asia/Tehran");
--- docker/html/view/dash_view.php
+++ docker/html/view/dash_view.php
@@ -20,7 +20,6 @@ function is_adm(): bool
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <style>
         #dr-bt{
             width: 90px;
